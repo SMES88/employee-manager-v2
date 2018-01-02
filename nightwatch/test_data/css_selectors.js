8497ae44d7ac8211d2b05c057b2eece67948c213
Dallas Killpack <Dallaskillpack@gmail.com>
2018-01-02 15:30:31 -0700

    more selectors and function

--- nightwatch/test_data/css_selectors.js
+++ nightwatch/test_data/css_selectors.js
@@ -1,22 +1,44 @@
 module.exports = {
-    titleBar: 'span[class="titleText"]',
-    employeeCard: 'div[class="infoCard"]',
-    noEmployeeMessage: 'p[id="noEmployee"]',
-    employeeList: 'ul[class="listContainer"]',
 
-    clearButton: 'button[name="clearSearch"]', 
-    searchBox: 'input[name="searchBox"]',
-    addEmployee: 'li[name="addEmployee"]',
+    buttons: {
+        clearButton: 'button[name="clearSearch"]',
+        deleteButton: 'button[name="delete"]',
+        addEmployee: 'li[name="addEmployee"]',
+        saveButton: 'button[name="save"]',
+        cancelButton: 'button[name="cancel"]',
+    },
+    
+    other: {
+        titleBar: 'span[class="titleText"]',
+        employeeList: 'ul[class="listContainer"]',
+        searchBox: 'input[name="searchBox"]',
+    },
 
-    cardTitle: 'p[name="employeeName"]',
-    idNumber: 'span[name="employeeID"]',
-    saveButton: 'button[name="save"]',
-    cancelButton: 'button[name="cancel"]',
-    nameLabel: 'span[name="nameLabel"]',
-    nameField: 'input[name="nameEntry"]',
-    phoneLabel: 'span[name="phoneLabel"]',
-    phoneField: 'input[name="phoneEntry"]',
-    titleLabel: 'span[name="titleLabel"]',
-    titleField: 'input[name="titleEntry"]'
+    cardInfo: {
+        employeeCard: 'div[class="infoCard"]',
+        noEmployeeMessage: 'p[id="noEmployee"]',
+        cardTitle: 'p[name="employeeName"]',
+        idNumber: 'span[name="employeeID"]',
+        nameLabel: 'span[name="nameLabel"]',
+        nameField: 'input[name="nameEntry"]',
+        phoneLabel: 'span[name="phoneLabel"]',
+        phoneField: 'input[name="phoneEntry"]',
+        titleLabel: 'span[name="titleLabel"]',
+        titleField: 'input[name="titleEntry"]',
+    },
+
+    employees: {
+        berniceOrtiz: 'li[name="employee1"]',
+        marnieBarnett: 'li[name="employee2"]',
+        phillipWeaver: 'li[name="employee3"]',
+        teresaOsborne: 'li[name="employee4"]',
+        dollieBerry: 'li[name="employee5"]',
+        harrietWilliamson: 'li[name="employee6"]',
+        rubyEstrada: 'li[name="employee7"]',
+        louWhite: 'li[name="employee8"]',
+        eveSparks: 'li[name="employee9"]',
+        loisBrewer: 'li[name="employee10"]',
+        addEmployee: 'li[name="employee11"]',
+    }
 
 }
