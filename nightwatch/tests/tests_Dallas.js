16cb88149e76046bf08d7b9a7efb5aca03cfd227
Your Name <you@example.com>
2018-01-03 10:34:18 -0700

    Finished tests :)

--- nightwatch/tests/tests_Dallas.js
+++ nightwatch/tests/tests_Dallas.js
@@ -1,54 +1,54 @@
-const selectors = require('../test_data/css_selectors')
-const data = require('../test_data/test_data')
-const functions = require('../test_data/functions')
+// const selectors = require('../test_data/css_selectors')
+// const data = require('../test_data/test_data')
+// const functions = require('../test_data/functions')
 
-module.exports = {
+// module.exports = {
 
-    before: browser => {
-        browser.url('http://localhost:3000')
-    },
-    after: browser => {
-        browser.end()
-    },
-    'Invalid info cant be saved': browser => {
-        browser
-            .click(selectors.employees.phillipWeaver)
-            .pause(100)
-        functions.enterValue(selectors.cardInfo.nameField, data.invalidData.name)
-        functions.enterValue(selectors.cardInfo.phoneField, data.invalidData.phone)
-        functions.enterValue(selectors.cardInfo.emailField, data.invalidData.email)
-        functions.enterValue(selectors.cardInfo.titleField, data.invalidData.title)
-        browser
-            .click(selectors.buttons.saveButton)
-            .pause(100)
-        browser.expect.element()
-    },
-    'Search function filters employee names': browser => {
-        functions.enterValue(selectors.other.searchBox, data.validData.name)
-        browser.expect.element(selectors.employees.phillipWeaver).text.to.equal(data.validData.name)
-        browser.expect.element(selectors.buttons.addEmployee).to.be.present;
-        browser.expect.element(selectors.employees.berniceOrtiz).to.not.to.be.present;
-        browser.expect.element(selectors.employees.marnieBarnett).to.not.to.be.present;
-        browser.expect.element(selectors.employees.teresaOsborne).to.not.to.be.present;
-        browser.expect.element(selectors.employees.dollieBerry).to.not.to.be.present;
-        browser.expect.element(selectors.employees.harrietWilliamson).to.not.to.be.present;
-        browser.expect.element(selectors.employees.rubyEstrada).to.not.to.be.present;
-        browser.expect.element(selectors.employees.louWhite).to.not.to.be.present;
-        browser.expect.element(selectors.employees.eveSparks).to.not.to.be.present;
-        browser.expect.element(selectors.employees.loisBrewer).to.not.to.be.present;
-        browser
-            .click(selectors.buttons.clearButton)
-            .pause(100)
-        browser.expect.element(selectors.employees.berniceOrtiz).to.be.present;
-        browser.expect.element(selectors.employees.marnieBarnett).to.be.present;
-        browser.expect.element(selectors.employees.phillipWeaver).to.be.present;
-        browser.expect.element(selectors.employees.teresaOsborne).to.be.present;
-        browser.expect.element(selectors.employees.dollieBerry).to.be.present;
-        browser.expect.element(selectors.employees.harrietWilliamson).to.be.present;
-        browser.expect.element(selectors.employees.rubyEstrada).to.be.present;
-        browser.expect.element(selectors.employees.louWhite).to.be.present;
-        browser.expect.element(selectors.employees.eveSparks).to.be.present;
-        browser.expect.element(selectors.employees.loisBrewer).to.be.present;
-        browser.expect.element(selectors.buttons.addEmployee).to.be.present;
-    }
-}
+//     before: browser => {
+//         browser.url('http://localhost:3000')
+//     },
+//     after: browser => {
+//         browser.end()
+//     },
+//     'Invalid info cant be saved': browser => {
+//         browser
+//             .click(selectors.employees.phillipWeaver)
+//             .pause(100)
+//         functions.enterValue(selectors.cardInfo.nameField, data.invalidData.name)
+//         functions.enterValue(selectors.cardInfo.phoneField, data.invalidData.phone)
+//         functions.enterValue(selectors.cardInfo.emailField, data.invalidData.email)
+//         functions.enterValue(selectors.cardInfo.titleField, data.invalidData.title)
+//         browser
+//             .click(selectors.buttons.saveButton)
+//             .pause(100)
+//         browser.expect.element()
+//     },
+//     'Search function filters employee names': browser => {
+//         functions.enterValue(selectors.other.searchBox, data.validData.name)
+//         browser.expect.element(selectors.employees.phillipWeaver).text.to.equal(data.validData.name)
+//         browser.expect.element(selectors.buttons.addEmployee).to.be.present;
+//         browser.expect.element(selectors.employees.berniceOrtiz).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.marnieBarnett).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.teresaOsborne).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.dollieBerry).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.harrietWilliamson).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.rubyEstrada).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.louWhite).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.eveSparks).to.not.to.be.present;
+//         browser.expect.element(selectors.employees.loisBrewer).to.not.to.be.present;
+//         browser
+//             .click(selectors.buttons.clearButton)
+//             .pause(100)
+//         browser.expect.element(selectors.employees.berniceOrtiz).to.be.present;
+//         browser.expect.element(selectors.employees.marnieBarnett).to.be.present;
+//         browser.expect.element(selectors.employees.phillipWeaver).to.be.present;
+//         browser.expect.element(selectors.employees.teresaOsborne).to.be.present;
+//         browser.expect.element(selectors.employees.dollieBerry).to.be.present;
+//         browser.expect.element(selectors.employees.harrietWilliamson).to.be.present;
+//         browser.expect.element(selectors.employees.rubyEstrada).to.be.present;
+//         browser.expect.element(selectors.employees.louWhite).to.be.present;
+//         browser.expect.element(selectors.employees.eveSparks).to.be.present;
+//         browser.expect.element(selectors.employees.loisBrewer).to.be.present;
+//         browser.expect.element(selectors.buttons.addEmployee).to.be.present;
+//     }
+// }
--- nightwatch/tests/tests_Joseph.js
+++ nightwatch/tests/tests_Joseph.js
@@ -1,44 +1,44 @@
-const selectors = require('../test_data/css_selectors')
-const data = require('../test_data/test_data')
-const functions = require('../test_data/functions')
+// const selectors = require('../test_data/css_selectors')
+// const data = require('../test_data/test_data')
+// const functions = require('../test_data/functions')
 
-module.exports = {
-    before: browser => {
-        browser.url('http://localhost:3000');
-    },
-    after: browser => {
-        browser.end();
-    },
-    'Testing the functionality of the save button EM1-21': browser => {
-        browser.click(selectors.employees.phillipWeaver)
-        functions.enterValue(selectors.cardInfo.nameField, data.validData.name, browser);
-        functions.enterValue(selectors.cardInfo.phoneField, data.validData.phone, browser);
-        functions.enterValue(selectors.cardInfo.emailField, data.validData.email, browser);
-        functions.enterValue(selectors.cardInfo.titleField, data.validData.title, browser);
-        browser.click(selectors.buttons.saveButton)
-    },
-    'Testing the cancel button EM1-17': browser => {
-        browser
-            .click(selectors.employees.phillipWeaver)
-        functions.enterEmployee(selectors.cardInfo, data.dummyData, browser)
-        browser
-            .click(selectors.buttons.cancelButton)
-        browser.expect.element(selectors.cardInfo.nameField).to.have.value.that.equals(data.validData.name)
-        browser.expect.element(selectors.cardInfo.phoneField).to.have.value.that.equals(data.validData.phone)
-        browser.expect.element(selectors.cardInfo.emailField).to.have.value.that.equals(data.validData.email)
-        browser.expect.element(selectors.cardInfo.titleField).to.have.value.that.equals(data.validData.title)
-    },
-    'Testing clicking to another user EM1-18': browser => {
-        browser
-            .click(selectors.employees.phillipWeaver)
-        functions.enterEmployee(selectors.cardInfo, data.dummyData, browser)
-        browser
-            .click(selectors.employees.berniceOrtiz)
-            .click(selectors.employees.phillipWeaver)
-        browser.expect.element(selectors.cardInfo.nameField).to.have.value.that.equals(data.validData.name)
-        browser.expect.element(selectors.cardInfo.phoneField).to.have.value.that.equals(data.validData.phone)
-        browser.expect.element(selectors.cardInfo.emailField).to.have.value.that.equals(data.validData.email)
-        browser.expect.element(selectors.cardInfo.titleField).to.have.value.that.equals(data.validData.title)
-    }
+// module.exports = {
+//     before: browser => {
+//         browser.url('http://localhost:3000');
+//     },
+//     after: browser => {
+//         browser.end();
+//     },
+//     'Testing the functionality of the save button EM1-21': browser => {
+//         browser.click(selectors.employees.phillipWeaver)
+//         functions.enterValue(selectors.cardInfo.nameField, data.validData.name, browser);
+//         functions.enterValue(selectors.cardInfo.phoneField, data.validData.phone, browser);
+//         functions.enterValue(selectors.cardInfo.emailField, data.validData.email, browser);
+//         functions.enterValue(selectors.cardInfo.titleField, data.validData.title, browser);
+//         browser.click(selectors.buttons.saveButton)
+//     },
+//     'Testing the cancel button EM1-17': browser => {
+//         browser
+//             .click(selectors.employees.phillipWeaver)
+//         functions.enterEmployee(selectors.cardInfo, data.dummyData, browser)
+//         browser
+//             .click(selectors.buttons.cancelButton)
+//         browser.expect.element(selectors.cardInfo.nameField).to.have.value.that.equals(data.validData.name)
+//         browser.expect.element(selectors.cardInfo.phoneField).to.have.value.that.equals(data.validData.phone)
+//         browser.expect.element(selectors.cardInfo.emailField).to.have.value.that.equals(data.validData.email)
+//         browser.expect.element(selectors.cardInfo.titleField).to.have.value.that.equals(data.validData.title)
+//     },
+//     'Testing clicking to another user EM1-18': browser => {
+//         browser
+//             .click(selectors.employees.phillipWeaver)
+//         functions.enterEmployee(selectors.cardInfo, data.dummyData, browser)
+//         browser
+//             .click(selectors.employees.berniceOrtiz)
+//             .click(selectors.employees.phillipWeaver)
+//         browser.expect.element(selectors.cardInfo.nameField).to.have.value.that.equals(data.validData.name)
+//         browser.expect.element(selectors.cardInfo.phoneField).to.have.value.that.equals(data.validData.phone)
+//         browser.expect.element(selectors.cardInfo.emailField).to.have.value.that.equals(data.validData.email)
+//         browser.expect.element(selectors.cardInfo.titleField).to.have.value.that.equals(data.validData.title)
+//     }
 
-}
+// }
